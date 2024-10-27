73c1ecd89afc27febe1aa8f61f9b88077633444e
drewnakamura <drnakamu@calpoly.edu>
2024-10-27 11:50:06 -0700

    Submit

--- data.py
+++ data.py
@@ -14,6 +14,28 @@ class Time:
         self.second = second
 
 
+    #This function inputs its own and another time, then returns the total combined time.
+    #Input other as Time
+    #Output newTime as Time
+    #def time_add(self, other):
+        #return newTime
+    #Input(Time(10, 10,10), Time(20, 20, 20)
+    #Output(TIme(30, 30, 30)
+    def time_add(self , other):
+
+        second = self.second + other.second
+        minutes = self.minute + other.minute
+        hours = self.hour + other.hour
+        if(second >= 60):
+            minutes+= second//60
+            second = second%60
+        if(minutes >= 60):
+            hours += minutes//60
+            minutes = minutes%60
+
+        newTime = Time(hours, minutes, second)
+        return newTime
+
     # Provide a developer-friendly string representation of the object.
     # input: Time for which a string representation is desired. 
     # output: string representation
@@ -23,7 +45,13 @@ class Time:
     # input: Time against which to compare
     # input: Another value to compare to the Time
     # output: boolean indicating equality
+    def __eq__(self, other:Any) -> bool:
+        return (other is self or
+                type(other) == Time and self.hour is other.hour and
+                self.minute is other.minute and self.second is other.second)
 
+    def __repr__(self) ->str:
+        return "The time is Hour: {} Minute: {} Second: {}".format(str(self.hour), str(self.minute), str(self.second))
 
 
 
@@ -53,3 +81,7 @@ class Point:
                 type(other) == Point and
                 math.isclose(self.x, other.x) and
                 math.isclose(self.y, other.y))
+
+
+
+
--- lab5_tests.py
+++ lab5_tests.py
@@ -14,17 +14,48 @@ class TestCases(unittest.TestCase):
 
 
     # Part 3
+    #### add_Time Tests
+    def test_add_Time_1(self):
+        time1 = data.Time(1, 2, 3)
+        time2 = data.Time(1, 2, 3)
+        time3 = lab5.time_add(time1, time2)
+        self.assertEqual("The time is Hour: 2 Minute: 4 Second: 6", repr(time3))
 
+    def test_add_Time_2(self):
+        time1 = data.Time(2, 65, 65)
+        time2 = data.Time(2, 65, 65)
+        time3 = lab5.time_add(time1, time2)
+        self.assertEqual("The time is Hour: 6 Minute: 12 Second: 10", repr(time3))
 
     # Part 4
+    def test_is_decending_1(self):
+        lista = [1.1, 2.1, 3.1, 4.1, 5.1]
+        self.assertEqual(False, lab5.is_decending(lista))
 
+    def test_is_decending_2(self):
+        lista = [5.1, 4.1, 3.1, 2.1, 1.1]
+        self.assertEqual(True, lab5.is_decending(lista))
 
     # Part 5
+    def test_largest_between_1(self):
+        lista = [1, 2, 3, 4, 5, 6, 7]
+        self.assertEqual(lab5.largest_between(lista, 2, 5), 5)
 
+    def test_largest_between_2(self):
+        lista = [1, 2, 3, 4, 5, 6, 7]
+        self.assertEqual(lab5.largest_between(lista, 3, 1), None)
 
     # Part 6
+    def test_furthest_1(self):
+        point1 = data.Point(2, 2)
+        point2 = data.Point(3, 3)
+        point3 = data.Point(4, 4)
+        list = [point1, point2, point3]
+        self.assertEqual(lab5.furthest_from_origin(list), 2)
 
-
+    def test_furthest_2(self):
+        list = []
+        self.assertEqual(lab5.furthest_from_origin(list), None)
 
 
 
